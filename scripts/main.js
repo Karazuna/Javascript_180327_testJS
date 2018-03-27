dc273c80f9f6ab2293edf46123778805f70377a0
Karazuna <kchy12345@gmail.com>
2018-03-27 16:52:27 +0900

    2018-03-27, JavaScript fucntion&imageChange

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
-function getIdPw(){
+function setIdPw() {
     var id = prompt('ID 입력', '');
     var passwd = prompt('PW 입력', '');
     localStorage.setItem('id', id);
     localStorage.setItem('passwd', passwd);
 }
 
-var passwd = localStorage.getItem('passwd');
-if(!passwd){
-    getIdPw();
+setIdPw();
+
+var myButton = document.querySelector('button');
+myButton.onclick = function () {
+    var roginId = prompt('ID 입력', '');
+    var roginPw = prompt('PW 입력', '');
+
+    if (roginId === localStorage.getItem('id')
+        && roginPw === localStorage.getItem('passwd')) {
+        alert("로그인 성공");
+        
+        var myHeading = document.querySelector('h1');
+        myHeading.innerHTML = roginId + "'s<br>HomePage입니다"
+    }
+    else {
+        alert("로그인 실패");
+    }
 }
 
-// var id = localStorage.getItem('id');
-// var heading = document.querySelector('h1');
-// heading.innerHTML = id + 'HomePage';
+
