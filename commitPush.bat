3812531dc7cc974ad46b9c723c93564dafd47a07
Karazuna <kchy12345@gmail.com>
2018-03-27 12:11:15 +0900

    2018-03-27, JavaScript fucntion&imageChange

--- commitPush.bat
+++ commitPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "2018-03-27, JavaScript Exercise_id&passwd"
+git commit -m "2018-03-27, JavaScript fucntion&imageChange"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,28 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Cloud Service<br>2018-1';
 
+function getIdPw() {
+    var id = prompt('ID 입력', ''); //초기값
+    alert(id + '가 로그인함');
+
+    var passwdSystem = '12345';
+    var passwd = prompt('PW 입력', '');
+
+    if (passwd === passwdSystem) {
+        alert('정상 로그인!');
+    }
+    else {
+        alert('비밀번호 오류');
+    }
+}
+
 /* var text = "This is a text~!@#";
 text = 12345;
 alert(text); */
 
 /* 
-1 == "1" : true
-1 === "1" : false
+1 == "1" : true (data check)
+1 === "1" : false (type/data check)
 */
 
 /* var iceCream = 'chocolate';
@@ -22,14 +37,21 @@ else{
     alert('Ahhhhha, chocolate........');
 } */
 
-var id = prompt('ID 입력', ''); //초기값
-alert(id + '가 로그인함');
+// getIdPw();
 
-var passwdSystem = '12345';
-var passwd = prompt('PW 입력', '');
-if(passwd == passwdSystem){
-    alert('정상 로그인!');
+//함수를 객체로 처리함
+document.querySelector('h1').onclick = function(){ //selector html, h1...
+    getIdPw();
 }
-else{
-    alert('비밀번호 오류');
+
+var myImg = document.querySelector('img');
+myImg.onclick = function(){
+    var src = myImg.getAttribute('src');
+    
+    if(src === 'images/firefox-icon.png'){
+        myImg.setAttribute('src', 'images/google.png');
+    }
+    else {
+        myImg.setAttribute('src', 'images/firefox-icon.png');
+    }
 }
