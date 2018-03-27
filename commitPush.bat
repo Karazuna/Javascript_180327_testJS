9487efd7b169f329828445cd67b87ccad3f4e4ea
Karazuna <kchy12345@gmail.com>
2018-03-27 10:50:14 +0900

    2018-03-27, JavaScript Exercise

---
+++ commitPush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "2018-03-27, JavaScript Exercise"
+git push -u origin master
---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+/*
+2018-03-27, 1260014 Cheol-Eon Kim
+*/
+
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloud Service<br>2018-1';
+
+/* var text = "This is a text~!@#";
+text = 12345;
+alert(text); */
+
+/* 
+1 == "1" : true
+1 === "1" : false
+*/
+
+/* var iceCream = 'chocolate';
+if(iceCream === 'chocolate'){
+    alert('chocolate ice cream?!');
+}
+else{
+    alert('Ahhhhha, chocolate........');
+} */
+
+var id = prompt('ID 입력', ''); //초기값
+alert(id + '가 로그인함');
